1b4e05ecfd1ddf4065f6edc47ffabf84f7ea6606
Vinicius Tavares <jedipinguim@gmail.com>
2019-05-25 23:29:19 -0300

    Primeiro Commit do projeto

---
+++ MenuSubmay/MenuMaker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MenuSubmay
+{
+    class MenuMaker
+    {
+        public Random Randomizer;
+
+        string[] Carnes = { "Carne", "Frango", "Atum" };
+        string[] Condimentos = { "Mostarda", "Mostarda Com Mel", "Pimenta Calabresa" };
+        string[] Paes = { "Branco", "Integral", "Italiano" };
+
+        public string GetMenuItem()
+        {
+            string randomCarnes = Carnes[Randomizer.Next(Carnes.Length)];
+            string randomCondimentos = Condimentos[Randomizer.Next(Condimentos.Length)];
+            string randomPaes = Paes[Randomizer.Next(Paes.Length)];
+
+            return randomCarnes + " com " + randomCondimentos + " no " + randomPaes;
+        }
+    }
+}
