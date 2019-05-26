6d87576ddd860614ea9071d564bba08b8658617c
Vinicius Tavares <jedipinguim@gmail.com>
2019-05-26 13:38:18 -0300

    igualando ao menu oficial do Subway

--- MenuSubmay/MenuMaker.cs
+++ MenuSubmay/MenuMaker.cs
@@ -8,17 +8,22 @@ namespace MenuSubmay
     {
         public Random Randomizer;
 
-        string[] Carnes = { "Carne", "Frango", "Atum" };
-        string[] Condimentos = { "Mostarda", "Mostarda Com Mel", "Pimenta Calabresa" };
-        string[] Paes = { "Branco", "Integral", "Italiano" };
+        string[] Carnes = { "Carne", "Pepperoni", "Frango", "Atum", "Frango Defumado com Cream Cheese", "Frango Empanado", "Frango Teriyaki" };
+        string[] Condimentos = { "Mostarda", "Molho Parmesão", "Cebola Agridoce", "Maionese", "Chipotle", "Barbecue" };
+        string[] Queijos = { "Cheddar", "Prato", "Suíço" };
+        string[] Vegetais = { "Alface Picada", "Tomates", "Azeitonas Pretas", "Picles", "Pimentões Verdes Crocantes", "Cebolas Fatiadas", "Pepino" };
+        string[] Paes = { "9 grãos", "9 grãos com aveia e mel", "Italiano", "Três queijos", "Parmesão e Orégano" };
 
         public string GetMenuItem()
         {
             string randomCarnes = Carnes[Randomizer.Next(Carnes.Length)];
             string randomCondimentos = Condimentos[Randomizer.Next(Condimentos.Length)];
+            string randomQueijos = Queijos[Randomizer.Next(Queijos.Length)];
+            string randomVegetais = Vegetais[Randomizer.Next(Vegetais.Length)];
             string randomPaes = Paes[Randomizer.Next(Paes.Length)];
 
-            return randomCarnes + " com " + randomCondimentos + " no " + randomPaes;
+            string escolha = $"{randomCarnes} com queijo {randomQueijos}, {randomVegetais} e {randomCondimentos} no pão {randomPaes}";
+            return escolha;
         }
     }
 }
